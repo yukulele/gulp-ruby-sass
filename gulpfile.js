f5afbc87a923df842f72f5053bb098c3e95010c1
Rob Wierzbowski <hello@robwierzbowski.com>
2014-10-18 21:28:49 -0400

    Clean up gulpfile for tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
-// var path = require('path');
-// var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
-// var debug = require('gulp-debug');
 var sass = require('./');
+// var debug = require('gulp-debug');
 
 gulp.task('sass', function() {
 	return sass('fixture/source', {
-		// bundleExec: true,
-		sourcemap: 'file',
-		noCache: true
+		sourcemap: 'file'
 	})
 	.on('error', function (err) {
 	  console.error('ERROR', err.message);
-	  // this.emit('end'); // good idea, should doc
    })
 	.pipe(sourcemaps.init({ loadMaps: true }))
 	.pipe(sourcemaps.write('../maps'))
-
-
-	// .pipe(debug({verbose:true}))
-	// .pipe(sourcemaps.write('../maps'))
 	.pipe(gulp.dest('fixture/results'));
 });
